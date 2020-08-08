55eae3437e635b4dcd647262b9b244c9438b2a0d
shaunak20 <42028795+shaunak20@users.noreply.github.com>
2020-08-08 13:50:54 +0530

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,89 @@
+
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  /* Smoothscroll js
+  -----------------------------------------------*/
+    $(function() {
+        $('.navbar-default a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+
+ /* Home Slideshow Vegas
+  -----------------------------------------------*/
+  $(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        autoPlay:true,
+        autoPlayTimeout:1000,
+        navClass : ['owl-prev','owl-next']
+        // "singleItem:true" is a shortcut for:
+        // items : 1, 
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+   
+    });
+   
+  });
+
+
+  /* Team carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+      $("#team-carousel").owlCarousel({
+          items : 1,
+          slideSpeed: 300,
+          autoPlay:true,
+          autoPlayTimeout:1000
+      });
+    });
+    
+
+    /* Back to Top
+    -----------------------------------------------*/
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+          $('.go-top').fadeIn(200);
+            } else {
+                $('.go-top').fadeOut(200);
+           }
+        });   
+          // Animate the scroll to top
+        $('.go-top').click(function(event) {
+          event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
